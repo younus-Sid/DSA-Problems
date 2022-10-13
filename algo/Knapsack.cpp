9809e63b536d59b76f413fd2b494f3ab535ec84a
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2022-10-13 11:33:27 +0530

    Create Knapsack.cpp

---
+++ algo/Knapsack.cpp
@@ -0,0 +1,5 @@
+/*******************************************************************************
+# What is Knapsack Problem?
+Knapsack ueses greedy approach to find th
+e optimal solution of a give problem.
+*******************************************************************************/
